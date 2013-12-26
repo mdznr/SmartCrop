d410c24534b3d2a443f99e2061531280f18a4a27
Matt Zanchelli <matt@mdznr.com>
2013-12-25 19:50:05 -0500

    Using toolbar instead of navigation bar for controls. Now showing action sheet for aspect ratios

--- PhotoThumbnail/MTZViewController.m
+++ PhotoThumbnail/MTZViewController.m
@@ -8,7 +8,11 @@
 
 #import "MTZViewController.h"
 
-@interface MTZViewController ()
+#import "MTZActionSheet.h"
+
+@interface MTZViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIToolbar *toolbar;
 
 @end
 
@@ -17,24 +21,102 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	
-	// Title
-	self.title = @"Thumbnails";
-	
-	// Image Picker Navigation Item
-	UIBarButtonItem *photos = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(didTapPhotosButton:)];
-	self.navigationItem.rightBarButtonItem = photos;
-	
-	// Show Navigation Bar
-	self.navigationController.navigationBarHidden = NO;
 }
 
+
+#pragma mark Toolbar Actions
+
 // Present Image Picker View Controller
-- (void)didTapPhotosButton:(id)sender
+- (IBAction)didTapPhotosButton:(id)sender
 {
 	UIImagePickerController *imgPicker = [[UIImagePickerController alloc] init];
+	imgPicker.delegate = self;
+#warning TODO: configure picker for demo application
 	[self presentViewController:imgPicker animated:YES completion:^{}];
 }
+
+// Present Action Sheet for Aspect Ratios
+- (IBAction)didTapActionButton:(id)sender
+{
+	// Create action sheet with title
+	MTZActionSheet *as = [[MTZActionSheet alloc] initWithTitle:@"Change the Photo's Aspect Ratio"];
+	
+	// Add buttons to action sheet
+	[as addButtonWithTitle:@"Original" andBlock:^{
+		[self changeAspectRatio:CGSizeZero];
+	}];
+	[as addButtonWithTitle:@"Square"   andBlock:^{
+		[self changeAspectRatio:(CGSize){1, 1}];
+	}];
+	[as addButtonWithTitle:@"3 × 2"    andBlock:^{
+		[self changeAspectRatio:(CGSize){3, 2}];
+	}];
+	[as addButtonWithTitle:@"3 × 5"    andBlock:^{
+		[self changeAspectRatio:(CGSize){3, 5}];
+	}];
+	[as addButtonWithTitle:@"4 × 3"    andBlock:^{
+		[self changeAspectRatio:(CGSize){4, 3}];
+	}];
+	[as addButtonWithTitle:@"4 × 6"    andBlock:^{
+		[self changeAspectRatio:(CGSize){4, 6}];
+	}];
+	[as addButtonWithTitle:@"5 × 7"    andBlock:^{
+		[self changeAspectRatio:(CGSize){5, 7}];
+	}];
+	[as addButtonWithTitle:@"8 × 10"   andBlock:^{
+		[self changeAspectRatio:(CGSize){8, 10}];
+	}];
+	[as addButtonWithTitle:@"16 × 9"   andBlock:^{
+		[self changeAspectRatio:(CGSize){16, 9}];
+	}];
+	as.cancelButtonTitle = @"Cancel";
+	
+	// Show action sheet
+	[as showFromToolbar:self.toolbar];
+}
+
+#pragma mark Change Aspect Ratio
+- (void)changeAspectRatio:(CGSize)aspectRatio
+{
+	// Handle Original
+	if ( CGSizeEqualToSize(aspectRatio, CGSizeZero) ) {
+#warning Handle Original Aspect Ratio
+		return;
+	}
+	
+	NSLog(@"%@", NSStringFromCGSize(aspectRatio));
+}
+
+
+#pragma mark UIImagePickerControllerDelgate
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+	// Dismiss the picker view
+	[picker dismissViewControllerAnimated:YES completion:^{}];
+	
+	/*
+	 Info Dictionary Keys:
+	 NSString *const UIImagePickerControllerMediaType;
+	 NSString *const UIImagePickerControllerOriginalImage;
+	 NSString *const UIImagePickerControllerEditedImage;
+	 NSString *const UIImagePickerControllerCropRect;
+	 NSString *const UIImagePickerControllerMediaURL;
+	 NSString *const UIImagePickerControllerReferenceURL;
+	 NSString *const UIImagePickerControllerMediaMetadata;
+	 */
+	
+	UIImage *editedImage = (UIImage *) [info objectForKey:UIImagePickerControllerEditedImage];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+	[picker dismissViewControllerAnimated:YES completion:^{}];
+}
+
+
+#pragma mark UIViewController Misc.
 
 - (void)didReceiveMemoryWarning
 {
