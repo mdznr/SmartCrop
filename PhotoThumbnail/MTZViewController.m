189f19ea139b1f8ec7834171fdc2f2bb513bb3a7
Matt Zanchelli <matt@mdznr.com>
2014-04-16 14:55:13 -0400

    Basic

--- PhotoThumbnail/MTZViewController.m
+++ PhotoThumbnail/MTZViewController.m
@@ -8,11 +8,17 @@
 
 #import "MTZViewController.h"
 
+#import "CGRectManipulation.h"
 #import "MTZActionSheet.h"
+#import "UIImage+AppropriateCrop.h"
 
 @interface MTZViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (strong, nonatomic) IBOutlet UIToolbar *toolbar;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIView *overlayView;
+@property (nonatomic) CGAspectRatio aspectRatio;
+@property (strong, nonatomic) UIImage *image;
 
 @end
 
@@ -21,6 +27,62 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+	
+	// Default aspect ratio
+//	_aspectRatio = CGAspectRatioZero;
+	_aspectRatio = (CGAspectRatio){0, 0};
+}
+
+
+#pragma mark Change Aspect Ratio / Update Photo Thumbnail
+
+- (void)changeAspectRatio:(CGAspectRatio)aspectRatio
+{
+	// Return early if value hasn't changed.
+	if ( CGAspectRatioEqualToAspectRatio(aspectRatio, _aspectRatio) ) {
+		return;
+	}
+	
+	_aspectRatio = aspectRatio;
+	
+	// Update the photo thumbnail with the new aspect ratio
+//	[self performSelectorInBackground:@selector(updatePhotoThumbnail) withObject:nil];
+	
+	// Update overlay mask with the crop region
+	[self performSelectorInBackground:@selector(updatePhotoOverlay) withObject:nil];
+}
+
+- (void)updatePhotoOverlay
+{
+	CGRect crop = [_image appropriateCropRegionForAspectRatio:_aspectRatio];
+	CGRect imageRect = (CGRect) {0, 0, _imageView.image.size.width, _imageView.image.size.height};
+	CGRect imageFrame = CGRectScaledRectToFitInRect(imageRect, _imageView.bounds);
+	imageFrame = CGRectCenterRectInRect(imageFrame, _imageView.bounds);
+	crop = CGRectScaledRectToFitInRect(crop, imageFrame);
+	NSLog(@"%@", NSStringFromCGRect(crop) );
+	
+	dispatch_async(dispatch_get_main_queue(), ^{
+//		UIBezierPath *outer = [UIBezierPath bezierPathWithRect:CGRectInfinite];
+		UIBezierPath *inner = [UIBezierPath bezierPathWithRect:crop];
+//		[outer appendPath:inner];
+		
+		CAShapeLayer *mask = [CAShapeLayer layer];
+		mask.path = inner.CGPath;
+		
+		_overlayView.layer.mask= mask;
+	});
+}
+
+- (void)updatePhotoThumbnail
+{
+/** TIME BEGIN **/ NSDate *d = [NSDate date];
+	UIImage *croppedImage = [_image appropriatelyCroppedImageForAspectRatio:_aspectRatio];
+/*** TIME END ***/ NSTimeInterval elapsedTime = [d timeIntervalSinceNow]; NSLog(@"%f", elapsedTime);
+	
+	// Update image on main thread
+	dispatch_async(dispatch_get_main_queue(), ^{
+		_imageView.image = croppedImage;
+	});
 }
 
 
@@ -39,54 +101,44 @@
 - (IBAction)didTapActionButton:(id)sender
 {
 	// Create action sheet with title
-	MTZActionSheet *as = [[MTZActionSheet alloc] initWithTitle:@"Change the Thumbnail's Aspect Ratio"];
+	MTZActionSheet *as = [[MTZActionSheet alloc] init];
+	as.title = @"Change the Thumbnail's Aspect Ratio";
 	
 	// Add buttons to action sheet
 	[as addButtonWithTitle:@"Original" andBlock:^{
-		[self changeAspectRatio:CGSizeZero];
+//		[self changeAspectRatio:CGAspectRatioZero];
+		[self changeAspectRatio:CGAspectRatioMake(0, 0)];
 	}];
 	[as addButtonWithTitle:@"Square"   andBlock:^{
-		[self changeAspectRatio:(CGSize){1, 1}];
+		[self changeAspectRatio:CGAspectRatioMake(1, 1)];
 	}];
 	[as addButtonWithTitle:@"3 × 2"    andBlock:^{
-		[self changeAspectRatio:(CGSize){3, 2}];
+		[self changeAspectRatio:CGAspectRatioMake(3, 2)];
 	}];
 	[as addButtonWithTitle:@"3 × 5"    andBlock:^{
-		[self changeAspectRatio:(CGSize){3, 5}];
+		[self changeAspectRatio:CGAspectRatioMake(3, 5)];
 	}];
 	[as addButtonWithTitle:@"4 × 3"    andBlock:^{
-		[self changeAspectRatio:(CGSize){4, 3}];
+		[self changeAspectRatio:CGAspectRatioMake(4, 3)];
 	}];
 	[as addButtonWithTitle:@"4 × 6"    andBlock:^{
-		[self changeAspectRatio:(CGSize){4, 6}];
+		[self changeAspectRatio:CGAspectRatioMake(4, 6)];
 	}];
 	[as addButtonWithTitle:@"5 × 7"    andBlock:^{
-		[self changeAspectRatio:(CGSize){5, 7}];
+		[self changeAspectRatio:CGAspectRatioMake(5, 7)];
 	}];
 	[as addButtonWithTitle:@"8 × 10"   andBlock:^{
-		[self changeAspectRatio:(CGSize){8, 10}];
+		[self changeAspectRatio:CGAspectRatioMake(8, 10)];
 	}];
 	[as addButtonWithTitle:@"16 × 9"   andBlock:^{
-		[self changeAspectRatio:(CGSize){16, 9}];
+		[self changeAspectRatio:CGAspectRatioMake(16, 9)];
 	}];
+	
+	// Add cancel button
 	as.cancelButtonTitle = @"Cancel";
 	
 	// Show action sheet
 	[as showFromBarButtonItem:sender animated:YES];
-}
-
-#pragma mark Change Aspect Ratio
-- (void)changeAspectRatio:(CGSize)aspectRatio
-{
-	// Handle Original
-	if ( CGSizeEqualToSize(aspectRatio, CGSizeZero) ) {
-#warning TODO: Handle Original Aspect Ratio
-		return;
-	}
-	
-#warning TODO: Handle Aspect Ratio Change
-	
-	NSLog(@"%@", NSStringFromCGSize(aspectRatio));
 }
 
 
@@ -98,18 +150,20 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
 	// Dismiss the picker view
 	[picker dismissViewControllerAnimated:YES completion:^{}];
 	
-	/*
-	 Info Dictionary Keys:
-	 NSString *const UIImagePickerControllerMediaType;
-	 NSString *const UIImagePickerControllerOriginalImage;
-	 NSString *const UIImagePickerControllerEditedImage;
-	 NSString *const UIImagePickerControllerCropRect;
-	 NSString *const UIImagePickerControllerMediaURL;
-	 NSString *const UIImagePickerControllerReferenceURL;
-	 NSString *const UIImagePickerControllerMediaMetadata;
-	 */
+	UIImage *image;
 	
-	UIImage *editedImage = (UIImage *) [info objectForKey:UIImagePickerControllerEditedImage];
+	// Get edited and cropped image
+	image = (UIImage *) [info objectForKey:UIImagePickerControllerEditedImage];
+	
+	// Otherwise get the original image
+	if ( !image ) {
+		image = (UIImage *) [info objectForKey:UIImagePickerControllerOriginalImage];
+	}
+	
+	_image = image;
+	
+	// Update the photo thumbnail with the new image
+	[self performSelectorInBackground:@selector(updatePhotoOverlay) withObject:nil];
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
@@ -119,6 +173,11 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info
 
 
 #pragma mark UIViewController Misc.
+
+- (BOOL)prefersStatusBarHidden
+{
+	return YES;
+}
 
 - (void)didReceiveMemoryWarning
 {
