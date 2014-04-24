efb4c063f0de8642fdd8eb26b55bb97339424a70
Matt Zanchelli <matt@mdznr.com>
2014-04-24 01:07:02 -0400

    Getting set up to test `CGRectManipulation`.

---
+++ Test Apps/SmartCrop/SmartCropTests/CGRectManipulationTests.m
@@ -0,0 +1,51 @@
+//
+//  CGRectManipulationTests.m
+//  SmartCrop
+//
+//  Created by Matt Zanchelli on 4/24/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "CGRectManipulation.h"
+
+@interface CGRectManipulationTests : XCTestCase
+
+@end
+
+@implementation CGRectManipulationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testCGRectOffsetRectToFitInRect
+{
+	
+}
+
+- (void)testCGRectScale
+{
+	
+}
+
+- (void)testCGRectScaledRectToFitInRect
+{
+	
+}
+
+- (void)testCGRectCenterRectInRect
+{
+	
+}
+
+@end
